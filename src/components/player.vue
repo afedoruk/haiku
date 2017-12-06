87d84d9b175d4a41eef7652f1669104c9b9bd060
Anna Fedoruk <afedoruk@gmail.com>
2017-12-06 23:13:23 +0300

    Добавлена переключалка музыки

--- src/components/player.vue
+++ src/components/player.vue
@@ -1,6 +1,6 @@
 <template>
   <p>
-    <audio :src="file" loop autoplay id="music-player"/>
+    <audio :src="file" loop autoplay ref="player"/>
     <button v-on:click="toggleMusic" class="btn btn-outline-secondary btn-sm" v-bind:class="{active: music}">Music</button>
   </p>
 </template>
@@ -8,8 +8,20 @@
 <script>
   export default {
     props: ['file'],
+    data() {
+      return {
+        music: true
+      }
+    },
     methods: {
       toggleMusic() {
+        if (this.music) {
+          this.$refs.player.pause()
+          this.music = false
+        } else {
+          this.$refs.player.play()
+          this.music = true
+        }
         console.log('toggle')
       }
     }
